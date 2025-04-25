c095da765e0201f2fbb9c6cdcaba0344d3e351ad
sam <42547923@qq.com>
2025-04-25 11:51:14 +0800

    Revert "Test git log"
    This reverts commit 3a4741a064328610453c19cd1553e60944ff3506.

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -24,15 +24,4 @@ class IndexController extends AbstractController
             'message' => "Hello {$user}.",
         ];
     }
-
-    public function test()
-    {
-        $user = $this->request->input('user', 'Test');
-        $method = $this->request->getMethod();
-
-        return [
-            'method' => $method,
-            'message' => "Hello {$user}.",
-        ];
-    }
 }
